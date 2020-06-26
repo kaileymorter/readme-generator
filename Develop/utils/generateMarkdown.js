98e715e3ab050b484eda8f30ae998d1df4a1f8c6
Spartacus <k_morter@outlook.com>
2020-06-26 13:35:48 -0600

    added function to write readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -24,15 +24,6 @@ function generateMarkdown(data) {
   ## Description:
   ${data.description}
 
-  ## Table of Contents:
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Credits](#credits)
-  * [License](#license)
-  * [Tests](#tests)
-  * [Questions](#questions)
-  
-
   ## Installation
   ${data.installation}
 
